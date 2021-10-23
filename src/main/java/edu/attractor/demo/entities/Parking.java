a63a09915243e29c9c87dadcc45e147bd5e229a6
eldiar <zyldyzbekoveldiiar@gmail.com>
2021-10-23 11:11:49 +0600

    initial commit

---
+++ src/main/java/edu/attractor/demo/entities/Parking.java
@@ -0,0 +1,20 @@
+package edu.attractor.demo.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
+public class Parking extends BaseEntity {
+    private Integer number;
+    private State state;
+}
---
+++ src/main/java/edu/attractor/demo/entities/User.java
@@ -0,0 +1,7 @@
+package edu.attractor.demo.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User extends BaseEntity {
+}
